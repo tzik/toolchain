3aa14927d9a6d169de2ded1004a76f101d28fcba
tzik <mail@tzik.jp>
2018-06-23 01:54:18 +0900

    Bump

--- env.sh
+++ env.sh
@@ -2,5 +2,11 @@ toolchain_dir="$(dirname "${BASH_SOURCE:-$0}")"
 path=("${toolchain_dir}/pkg/depot_tools"(N) "${path[@]}")
 path=("${toolchain_dir}/out/usr/bin"(N) "${path[@]}")
 
+if [ "${#toolchain_dir}" -ne 0 ]; then
+  path_map[${toolchain_dir}]="tc"
+  path_map[${toolchain_dir}/pkg/llvm/src/llvm]="llvm"
+  path_map[${toolchain_dir}/pkg/llvm/src/clang]="clang"
+fi
+
 # TODO: Build gn by ourselves.
 export CHROMIUM_BUILDTOOLS_PATH="${toolchain_dir}/pkg/v8/v8/buildtools"
