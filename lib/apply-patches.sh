f08fcd4069aafde8aee84a9f6e19f5a4867e03b4
tzik <main@tzik.jp>
2019-01-17 14:06:31 +0900

    Bump

--- lib/apply-patches.sh
+++ lib/apply-patches.sh
@@ -1,6 +1,7 @@
 shopt -s nullglob
 for i in "${base_dir}/patches"/*.patch; do
   if ! patch -d "${src_dir}" -R -f -s -p1 --dry-run -i "$i" > /dev/null 2>&1; then
+    echo "Applying $(basename "$i")"
     patch -d "${src_dir}" -N -f -s -p1 -i "$i"
   fi
 done
